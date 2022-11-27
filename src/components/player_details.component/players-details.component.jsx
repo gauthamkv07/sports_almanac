ae9f86266ef2cb00093511a4a34349d6b68b907c
HariVigneshG123 <86461612+HariVigneshG123@users.noreply.github.com>
2022-11-27 02:01:28 -0500

    temp commit

--- src/components/player_details.component/players-details.component.jsx
+++ src/components/player_details.component/players-details.component.jsx
@@ -9,14 +9,12 @@ const PlayerDetailsComponent = ({ data }) => {
                 <img className="player-img" src={data.player.photo} alt="#" />
             </div>
             <div className="text-image">
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Full Name:&nbsp;</div>{data.player.firstname}{" "}{data.player.lastname}</div></PlayerDetailRow>
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Nationality:&nbsp;</div>{data.player.nationality}</div></PlayerDetailRow>
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Age:&nbsp;</div>{data.player.age}</div></PlayerDetailRow>
-            </div>
-            <div className="text-image">
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Club:&nbsp;</div>{data.statistics[0].team.name}</div></PlayerDetailRow>
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Position:&nbsp;</div>{data.statistics[0].games.position}</div></PlayerDetailRow>
-                <PlayerDetailRow><div className="stat-space"><div className="header-text">Date of Birth:&nbsp;</div>{data.player.birth.date}</div></PlayerDetailRow>
+                <PlayerDetailRow>Full Name:{data.player.firstname}{" "}{data.player.lastname}</PlayerDetailRow>
+                <PlayerDetailRow>Nationality:{data.player.nationality}</PlayerDetailRow>
+                <PlayerDetailRow>Age:{data.player.age}</PlayerDetailRow>
+                <PlayerDetailRow>Club:{data.statistics[0].team.name}</PlayerDetailRow>
+                <PlayerDetailRow>Position:{data.statistics[0].games.position}</PlayerDetailRow>
+                <PlayerDetailRow>Date of Birth{data.player.birth.date}</PlayerDetailRow>
             </div>
         </div>
     )
--- src/components/player_details.component/stats-compressed.component.jsx
+++ src/components/player_details.component/stats-compressed.component.jsx
@@ -9,7 +9,6 @@ const Statscompressed = ({ data }) => {
                 <div className='arrow-space'><img className="expand-arrow" src={expandArrow} alt="logo" /></div>
                 <h3 className='stats-league-name'>{data.league.name}</h3>
                 <div className='stat-space'><div className='header-text'>Appearances:&nbsp;</div>{data.games.appearences ? data.games.appearences : 0}</div>
-                <div className="stat-space"><div className="header-text">Goals:&nbsp;</div>{data.goals.total ? data.goals.total : 0}</div>
                 <div className='stat-space'><div className='header-text'>Average Rating:&nbsp;</div>{data.games.rating ? data.games.rating.substring(0, 4) : 0}</div>
             </div>
         </div>
